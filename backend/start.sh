ddb3e1fbf3e55e4772b38a44c3aaa913a10021af
Ali Zain <ali.zain@zweidevs.com>
2026-04-09 09:57:36 +0500

    Hot Fix: Fix deployment

--- backend/start.sh
+++ backend/start.sh
@@ -12,5 +12,4 @@ import alembic.config
 alembic.config.main(argv=['upgrade', 'head'])
 "
 
-python -m src/atlas/seed
-python -m uvicorn atlas.main:create_app --factory --host 0.0.0.0 --port 8080
+python -m pip install . && PYTHONPATH=/app/src python -m alembic upgrade head && PYTHONPATH=/app/src python -m atlas.seed && PYTHONPATH=/app/src python -m uvicorn atlas.main:create_app --factory --host 0.0.0.0 --port 8080
